350e0348dc5f1ff4560a1794ef6eeb3d8d154cae
Amy <amysun.uk@gmail.com>
2024-08-05 11:10:55 +0100

    Fizz if multiples of 3, Buzz if multiples of 5, FizzBuzz if both.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+IEnumerable<int> numbers = Enumerable.Range(1, 100);
+foreach (int num in numbers)
+{
+    if (num % 3 == 0 && num % 5 == 0)
+    {Console.WriteLine("FizzBuzz");}
+    else if (num % 3 == 0)
+    {Console.WriteLine("Fizz");}
+    else if (num % 5 == 0)
+    {Console.WriteLine("Buzz");}
+    else
+    {Console.WriteLine(num);}
+}
--- obj/Debug/net8.0/fizzbuzz-cs.AssemblyInfo.cs
+++ obj/Debug/net8.0/fizzbuzz-cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fizzbuzz-cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cacf86a19866971fbb3ad290b12889442c6b66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("fizzbuzz-cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fizzbuzz-cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
